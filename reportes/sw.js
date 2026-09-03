3a8c473288cad48002b1f93c51a46ed6069f9cfa
juniorgoiris07-cmyk <juniorgoiris07@gmail.com>
2026-09-03 07:47:29 -0300

    Modo claro forzado, quitar engranaje, cache v2

--- reportes/sw.js
+++ reportes/sw.js
@@ -5,7 +5,7 @@
 // datos y cola de ediciones sin conexión vive en index.html, no en el
 // service worker.
 
-const CACHE_NAME = 'stock-reportes-v1';
+const CACHE_NAME = 'stock-reportes-v2';
 const ASSETS = [
   './',
   './index.html',
